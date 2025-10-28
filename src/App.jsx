651b92f666ad6f1bee9aea4a19b3456a98479558
Muluneh Meri <muluneh.meri@eaglelionsystems.com>
2025-10-28 11:39:41 +0300

    update loading animation on landing page

--- src/App.jsx
+++ src/App.jsx
@@ -132,7 +132,7 @@ export default function App() {
             {loading && (
                 <div className="loading">
                     <div className="spinner"></div>
-                    <p>Loading quiz from Google Drive... ⏳</p>
+                    <p>Loading quiz Avaliable ... ⏳</p>
                 </div>
             )}
 
